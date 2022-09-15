5dad265ad46968efe8e2de1e0b1a3b6360388e7c
Tolani Daniel <mumytosin2016@gmail.com>
2022-09-15 13:43:25 +0100

    update: Working animation of cube - Rotation

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
-import { Group } from 'three';
+import { Group, Mesh } from 'three';
 
 
 // I need a mesh
@@ -13,72 +13,73 @@ import { Group } from 'three';
 const scene = new THREE.Scene()
 // I need to place a mesh in the scene
 
+// console.log(date.getMinutes())
 // // Geometry 
 // // Since I am trying to create a cube, best THREE.js geometry to use is the BOXGeometry which takes in three (3) values.
 // // The BoxGeometry takes in three values for x-y-z axis
-// const geometry = new THREE.BoxGeometry(1,1,1)
+const geometry = new THREE.BoxGeometry(1,1,1)
 
-// // // Material
-// // // There are different types of material, but, just like nodes in blender, 
-// // //       we can add differnet types of materials in THREE.js, we currently use the MeshBasicMaterial since we just want to slap in a single color
-// // //       The MeshBasicMaterial takes in properties like 'color'
-// const material = new THREE.MeshBasicMaterial( { color: 0xe4ad7b } )
+// // Material
+// // There are different types of material, but, just like nodes in blender, 
+// //       we can add differnet types of materials in THREE.js, we currently use the MeshBasicMaterial since we just want to slap in a single color
+// //       The MeshBasicMaterial takes in properties like 'color'
+const material = new THREE.MeshBasicMaterial( { color: 0xe4ad7b } )
 
-// // // NOW, we can create our mesh which will take in the geometry and the material
-// const cubeMesh = new THREE.Mesh(geometry, material)
-// // // We need to add this mesh we have created too the scene
+// // NOW, we can create our mesh which will take in the geometry and the material
+const cubeMesh = new THREE.Mesh(geometry, material)
+// // We need to add this mesh we have created too the scene
 
-// // // REORDER - Comes before the vector3D properties
-// // // A string must be passed in - telling how the arrangment should tae place
+// // REORDER - Comes before the vector3D properties
+// // A string must be passed in - telling how the arrangment should tae place
 // cubeMesh.rotation.reorder('YXZ')
 // cubeMesh.rotation.x = 120
 // cubeMesh.rotation.y = 120
 // cubeMesh.rotation.z = 120
 
-// // // Use of SET - reduce the XYZ properties of all Object3D class inheritance into one line of 3 parameters
-// // // cubeMesh.rotation.set(3,4,1)
-// // cubeMesh.position.set(0.8,0.2,1)
+// // Use of SET - reduce the XYZ properties of all Object3D class inheritance into one line of 3 parameters
+// // cubeMesh.rotation.set(3,4,1)
+// cubeMesh.position.set(0.8,0.2,1)
 
-// // //NORMALIZE
-// cubeMesh.position.normalize()
+// //NORMALIZE
+cubeMesh.position.normalize()
 
-// // LENGTH Method for position 
+// LENGTH Method for position 
 // console.log(cubeMesh.position.length())
 
 // console.log(cubeMesh.position.distanceTo(new THREE.Vector3(1,1,3)))
 
 // console.log(cubeMesh.position.distanceTo(camera.position))
 
-// scene.add(cubeMesh)
+scene.add(cubeMesh)
 
 
 // CREATING A GROUP
-const cubeGroup = new THREE.Group();
-scene.add(cubeGroup)
+// const cubeGroup = new THREE.Group();
+// scene.add(cubeGroup)
 
-const mesh1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial( { color: 0x0000FF})
-)
+// const mesh1 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1,1,1),
+//     new THREE.MeshBasicMaterial( { color: 0x0000FF})
+// )
 
-const mesh2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial( { color: 0x00FF00})
-)
+// const mesh2 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1,1,1),
+//     new THREE.MeshBasicMaterial( { color: 0x00FF00})
+// )
 
-mesh2.position.set(1.3,0,0)
+// mesh2.position.set(1.3,0,0)
 
-const mesh3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial( { color: 0xFF0000})
-)
+// const mesh3 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1,1,1),
+//     new THREE.MeshBasicMaterial( { color: 0xFF0000})
+// )
 
-mesh3.position.set(-1.3,0,0)
+// mesh3.position.set(-1.3,0,0)
 
-cubeGroup.add(mesh1, mesh2, mesh3)
+// cubeGroup.add(mesh1, mesh2, mesh3)
 
-cubeGroup.scale.y = 2
-cubeGroup.rotation.y = 1
+// cubeGroup.scale.y = 2
+// cubeGroup.rotation.y = 1
 
 // cubeGroup.rotation.z = 2
 // We now need a camera to serve as our eyes in this scene
@@ -88,7 +89,6 @@ cubeGroup.rotation.y = 1
 
 // We need to declare the size for the aspect ratio - In form objects where we assign a width and a height value.
 // There are no specified units of measurement for the width and height
-
 
 
 const sizes = {
@@ -145,4 +145,22 @@ renderer.setSize(
     sizes.width, sizes.height
 )
 
-renderer.render(scene, camera)
+
+
+
+// ANIMATION
+// Request Animation Frame calls a function on each frame
+const tickCalled = () => {
+
+    console.log("Hello")
+    cubeMesh.rotation.y += 0.01
+    console.log(cubeMesh.position.distanceTo(camera.position))
+    renderer.render(scene, camera)
+
+    // const time = Date.now()
+    // console.log(time)
+
+    window.requestAnimationFrame(tickCalled)
+}
+
+tickCalled();
